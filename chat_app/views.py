950e828eca3f17f597bea55c8af08cb8f5b422f1
Manish <manishpandit9867@gmail.com>
2026-04-02 23:01:30 +0545

    Addinf update function and update templates

--- chat_app/views.py
+++ chat_app/views.py
@@ -26,4 +26,14 @@ def chat_create(request):
         if message_text:
             Message.objects.create(sender=sender,message=message_text)
     return redirect('chat-list')
+
+def chat_update(request,id):
+    chat=get_object_or_404(Message,id=id)
+    if request.method=="POST":
+        message_text=request.POST.get("message")
         
+        if message_text:
+            chat.message=message_text
+            chat.save()
+    return redirect('chat-list')
+    
